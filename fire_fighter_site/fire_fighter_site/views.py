c791777c6738f7d926702fada0a9e3b005b2ebb1
Gregory Klupar <Klupamos@gmail.com>
2013-03-26 03:03:29 -0800

    I added javascript to base_template.djt that creates a cookies. This cookie will be used in the information modification viees. I changed the default ajax response in include an error message. I changed the create_navlinks function to take a user object instead of the whole request object

--- fire_fighter_site/fire_fighter_site/views.py
+++ fire_fighter_site/fire_fighter_site/views.py
@@ -8,17 +8,17 @@ class anchor(object):
         self.href = href
         self.text = text
 
-def create_navlinks(request):
+def create_navlinks(user):
     nav_links = []
     # Note these hardcoded if statments need to be changed out for permissions
-    if request.user.is_authenticated():
+    if user.is_authenticated():
         nav_links.append(anchor("/account_info","Account Information"))
         nav_links.append(anchor("/account_certs", "Account Certificates"))
-        if request.user.username == 'train':
+        if user.username == 'train':
             nav_links.append(anchor("/training", "Training Officer"))
-        if request.user.username == 'certify':
+        if user.username == 'certify':
             nav_links.append(anchor("/certifying", "Certifying Officer"))
-        if request.user.username == 'admin':
+        if user.username == 'admin':
             nav_links.append(anchor("/admin", "Administration"))
         nav_links.append(anchor("/public_certs", "view certificates"))
         nav_links.append(anchor("/logout", "logout"))
@@ -32,7 +32,7 @@ from django.contrib.auth import logout
 from django.core.context_processors import csrf
 def login_view(request):
 
-    if (request.user.is_authenticated()):
+    if (user.is_authenticated()):
         return redirect('/account_info')
 
     sent_username = request.POST.get('username', 'default')
@@ -50,7 +50,7 @@ def login_view(request):
 
     template_file = 'login_template.djt'
     context_dict['path'] = request.path
-    context_dict['nav_links'] = create_navlinks(request)
+    context_dict['nav_links'] = create_navlinks(request.user)
     context_dict.update(csrf(request))
     return render_to_response(template_file, context_dict)
 
@@ -59,36 +59,36 @@ def logout_view(request):
     return redirect('/login')
 
 def create_account_view(request):
-    nav_links = create_navlinks(request)
+    nav_links = create_navlinks(request.user)
     context_dict = {'nav_links': nav_links}
     return render_to_response('create_account_template.djt', context_dict)
 
 def public_certs_view(request):
-    nav_links = create_navlinks(request)
+    nav_links = create_navlinks(request.user)
     context_dict = {'nav_links': nav_links}
     return render_to_response('public_certs_template.djt', context_dict)
 
 def account_info_view(request):
-    nav_links = create_navlinks(request)
+    nav_links = create_navlinks(request.user)
     context_dict = {'nav_links': nav_links}
     return render_to_response('account_info_template.djt', context_dict)
 
 def account_certs_view(request):
-    nav_links = create_navlinks(request)
+    nav_links = create_navlinks(request.user)
     context_dict = {'nav_links': nav_links}
     return render_to_response('account_certs_template.djt', context_dict)
 
 def training_view(request):
-    nav_links = create_navlinks(request)
+    nav_links = create_navlinks(request.user)
     context_dict = {'nav_links': nav_links}
     return render_to_response('training_template.djt', context_dict)
 
 def certifying_view(request):
-    nav_links = create_navlinks(request)
+    nav_links = create_navlinks(request.user)
     context_dict = {'nav_links': nav_links}
     return render_to_response('certifying_template.djt', context_dict)
 
 def admin_view(request):
-    nav_links = create_navlinks(request)
+    nav_links = create_navlinks(request.user)
     context_dict = {'nav_links': nav_links}
     return render_to_response('admin_template.djt', context_dict)
