9555eb1d65bed8fa13d26ad7ffc1a0b70f4bb39f
Gregory Klupar <Klupamos@gmail.com>
2013-03-28 23:33:54 -0800

    rearanged some of the foreign keys, and gave them related names to all relations
    Signed-off-by: Gregory Klupar <Klupamos@gmail.com>

--- fire_fighter_site/candidate/models.py
+++ fire_fighter_site/candidate/models.py
@@ -3,13 +3,12 @@ from django.db import models
 ##### TO DO #####
 #1. Change ER diagram to allow forign key refence between Jurisdiction and User in terms of TA/CO
 #2. Need to intigrate User and Candidate into contrib.auth app
-#3. Add permissions (see: https://docs.djangoproject.com/en/dev/ref/models/options/#django.db.models.Options.permissions)
 
 class Certification(models.Model):
-    #not sure need clarification on length 
     name = models.CharField(max_length=50, unique=True)
     description = models.CharField(max_length=1000)
-    
+    requirements = models.ManyToManyField('Requirement', related_name='certifications')
+
     def __unicode__(self):
         return self.name
     
@@ -18,16 +17,16 @@ class Certification(models.Model):
     
 class Requirement(models.Model):
     name = models.CharField(max_length=50, unique=True)
-    certification = models.ForeignKey(Certification)
 
     def __unicode__(self):
         return self.name
     
     class Meta:
         ordering = ["name"]
-    
+
+# will be used for authentication
 class User(models.Model):
-    candidate = models.ForeignKey('Candidate', on_delete=models.CASCADE)
+    candidate = models.ForeignKey('Candidate', related_name='auth')
     username = models.CharField(max_length=64, unique=True)
     password = models.CharField(max_length=64)
     STATUS_TYPES = (('P','Pending'),('A','Active'),('R','Retired'),)
@@ -39,7 +38,6 @@ class User(models.Model):
     class Meta:
         order_with_respect_to = 'candidate'
 
-    
 class Candidate(models.Model):
     fire_fighter_ID = models.CharField(max_length=10, unique=True) #need to add somthing here to make sure that user does not put in an arbitrary FF ID, or make this a foregin key with some kind of look up in the implementation
     first_name = models.CharField(max_length=50)
@@ -52,25 +50,25 @@ class Candidate(models.Model):
     #Do not need to validate these three here, see django.contrib.localflavor
     street_address = models.CharField(max_length=50)
     city = models.CharField(max_length=25)
-    zip_code = models.CharField(max_length=6)
+    zip_code = models.CharField(max_length=10)
     state = models.CharField(max_length=20)
     
-    jurisdiction = models.ForeignKey('Jurisdiction')
-    certifications = models.ManyToManyField(Certification)
-    requirements = models.ManyToManyField(Requirement)
+    jurisdiction = models.ForeignKey('Jurisdiction', related_name='candidate_list')
+    certifications = models.ManyToManyField(Certification, related_name='completed_by+')
+    requirements = models.ManyToManyField(Requirement, related_name='completed_by+')
 
     def __unicode__(self):
-        return self.firstname + ' '+ self.middle_initial + ' ' + self.last_name + ' ' + self.suffix
+        return " ".join([self.first_name, self.middle_initial, self.last_name, self.last_name, self.suffix])
     
     class Meta:
         ordering = ["last_name", "first_name"]
-    
+        unique_together = ('first_name','middle_initial','last_name','suffix')
     
     
 class Jurisdiction(models.Model):
     name = models.CharField(max_length=50, unique=True)
-    training_officer = models.ManyToManyField(Candidate, related_name='_TO_')
-    certifying_officer = models.ManyToManyField(Candidate, related_name='_CO_')
+    training_officer = models.ManyToManyField(Candidate, related_name='trains_jurisdiction')
+    certifying_officer = models.ManyToManyField(Candidate, related_name='certifies_jurisdiction')
 
     def __unicode__(self):
         return self.name
@@ -78,9 +76,9 @@ class Jurisdiction(models.Model):
     class Meta:
         ordering  = ["name"]    
     
-class Transfer_Request(models.Model):                # candidate wants to move jurisdictions
-    candidate    = models.ForeignKey(Candidate)
-    jurisdiction = models.ForeignKey(Jurisdiction)
+class Transfer_Request(models.Model):
+    candidate    = models.ForeignKey(Candidate, related_name='transfer_request', unique=True)
+    jurisdiction = models.ForeignKey(Jurisdiction, related_name='requested_transfers', primary_key=True)
     TO_approval  = models.BooleanField(default=False)
 
     def __unicode__(self):
